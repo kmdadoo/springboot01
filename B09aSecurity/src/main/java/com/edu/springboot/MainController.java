822aff897d8dd3eba51996153c31de82c8a7e7a4
kmdadoo1 <kmdadoo1@gmail.com>
2025-06-16 17:36:55 +0900

    4일차

---
+++ B09aSecurity/src/main/java/com/edu/springboot/MainController.java
@@ -0,0 +1,35 @@
+package com.edu.springboot;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController
+{
+	@RequestMapping("/")
+	public String home()
+	{
+		return "home";
+	}
+	
+	//guest 하위는 권한없이 누구나 접근할 수 있는 영역 
+	@RequestMapping("/guest/index.do")
+	public String welocome1() 
+	{
+		return "guest";
+	}
+	
+	//member 하위는 ADMIN 혹은 USER 권한을 획득 후 접근 가능
+	@RequestMapping("/member/index.do")
+	public String welocome2() 
+	{
+		return "guest";
+	}
+	
+	//admin 하위는 ADMIN 권한만 접근할 수 있는 영역으로 설정 
+	@RequestMapping("/admin/index.do")
+	public String welocome3() 
+	{
+		return "guest";
+	}
+}
